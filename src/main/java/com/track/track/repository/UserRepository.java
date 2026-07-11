e587b91f97bee77bbc27274a3f667d04ba1f9244
electrum21 <tjmjavier@gmail.com>
2026-07-11 17:28:24 +0800

    add functions for sending reminders to telegram bot

--- src/main/java/com/track/track/repository/UserRepository.java
+++ src/main/java/com/track/track/repository/UserRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
     Optional<User> findByFirebaseUid(String firebaseUid);
     Optional<User> findByTelegramChatId(String telegramChatId);
+    List<User> findByTelegramChatIdIsNotNull();
 }
